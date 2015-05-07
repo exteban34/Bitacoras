e4d277e9acbfcdadbe3af3528195bd6159b4ad84
Esteban Alvarez <exteban34@gmail.com>
2015-05-07 15:37:40 -0500

    Agregar funcionalidad que comprueba la conexion a una red

--- src/com/edu/udea/drai/bitacoras/MainActivity.java
+++ src/com/edu/udea/drai/bitacoras/MainActivity.java
@@ -1,6 +1,5 @@
 package com.edu.udea.drai.bitacoras;
 
-
 import java.util.ArrayList;
 
 import org.json.JSONArray;
@@ -10,12 +9,14 @@ import com.edu.udea.drai.bitacoras.model.Tarea;
 import com.edu.udea.drai.bitacoras.util.DataPass;
 import com.edu.udea.drai.bitacoras.util.LeerJSON;
 
-
-
-
 import android.app.Activity;
+import android.app.AlertDialog;
 import android.app.ProgressDialog;
+import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.View;
@@ -23,16 +24,14 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
-
-
 /**
- * MainActivity, provee un espacio para ingresar la cedula de la cual se quiere consultar
- * las tareas registradas en el sistema                        
+ * MainActivity, provee un espacio para ingresar la cedula de la cual se quiere
+ * consultar las tareas registradas en el sistema
+ * 
  * @author Heinner Esteban Alvarez <exteban34@gmail.com>
  * @version 1.0 26/03/2015
  */
 public class MainActivity extends Activity {
-
 
 	/**
 	 * Elementos de la vista
@@ -43,32 +42,31 @@ public class MainActivity extends Activity {
 	 * String con la cedula digitada
 	 */
 	String cedAux;
-	
-	
+
 	/**
 	 * ProgressDialog que indica la carga de datos
 	 */
 	ProgressDialog pDialog;
-	
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 		boton_consulta = (Button) findViewById(R.id.botonConsulta);
 		edCedula = (EditText) findViewById(R.id.editTextCedula);
-	
-	}
-	
-	
-	public void onClickConsultar(View view){
-		cedAux=edCedula.getText().toString();
-		edCedula.setText("");
-		new LeerAuxiliarCedula().execute("http://172.21.32.77:1350/calendario/");
-		
-		
+
 	}
 
-	
+	public void onClickConsultar(View view) {
+		cedAux = edCedula.getText().toString();
+		edCedula.setText("");
+		if (estaConectado()) {
+			new LeerAuxiliarCedula()
+					.execute("http://172.21.32.77:1350/calendario/");
+		}
+
+	}
+
 	private class LeerAuxiliarCedula extends AsyncTask<String, Void, String> {
 		protected void onPreExecute() {
 			pDialog = new ProgressDialog(MainActivity.this);
@@ -83,56 +81,118 @@ public class MainActivity extends Activity {
 		}
 
 		protected void onPostExecute(String result) {
-		 try {		 			 	
-			 	pDialog.dismiss();
-			 	/**
+			try {
+				pDialog.dismiss();
+				/**
 				 * Obtengo listado de tareas del auxiliar
-				 */	
-			 	ArrayList<Tarea> tareasObj = new ArrayList<Tarea>();
-			 	
-			 	JSONArray tareas= new JSONArray(result);
-			 	
-			 	for (int i = 0; i < tareas.length(); i++) {
-			 		JSONObject tareaJSON = tareas.getJSONObject(i);			 		
-			 		if(tareaJSON.getString("cedulaaux").equalsIgnoreCase(cedAux)){			 			
-			 			Tarea tarea = new Tarea(tareaJSON.getString("id"),
-									tareaJSON.getString("auxiliar"),
-									tareaJSON.getString("cedulaaux"),
-									tareaJSON.getString("fecha"),
-									tareaJSON.getString("horario"),
-									tareaJSON.getString("tarea"),
-									tareaJSON.getString("perfil"),
-									tareaJSON.getString("observacion")									
-									);	
-			 						tareasObj.add(tarea);		
-			 								
-			 			}
-			 			 		
-			 				 		
-			 		} 
-			 		
-			 		
-			 		if (tareasObj.isEmpty()){
-			 			Toast.makeText(getApplicationContext(), "No existe el usuario.  \n"
-								+ "Por favor verifique la informacion ingresada",
-								Toast.LENGTH_LONG).show();
-			 			
-			 		}else{
-			 			Intent e = new Intent(
-			 					"com.edu.udea.drai.bitacoras.Lista_tareas");
-			 			e.putExtra("tareas", new DataPass(tareasObj));
-			 			startActivity(e);
-			 		}
-			 		
-			 	
-			 			 		
-			 
-			 	} catch (Exception e) {
-			 		e.printStackTrace();
-			 	}
-			 }
+				 */
+				ArrayList<Tarea> tareasObj = new ArrayList<Tarea>();
+
+				JSONArray tareas = new JSONArray(result);
+
+				for (int i = 0; i < tareas.length(); i++) {
+					JSONObject tareaJSON = tareas.getJSONObject(i);
+					if (tareaJSON.getString("cedulaaux").equalsIgnoreCase(
+							cedAux)) {
+						Tarea tarea = new Tarea(tareaJSON.getString("id"),
+								tareaJSON.getString("auxiliar"),
+								tareaJSON.getString("cedulaaux"),
+								tareaJSON.getString("fecha"),
+								tareaJSON.getString("horario"),
+								tareaJSON.getString("tarea"),
+								tareaJSON.getString("perfil"),
+								tareaJSON.getString("observacion"));
+						tareasObj.add(tarea);
+
+					}
+
+				}
+
+				if (tareasObj.isEmpty()) {
+					Toast.makeText(
+							getApplicationContext(),
+							"No existe el usuario.  \n"
+									+ "Por favor verifique la informacion ingresada",
+							Toast.LENGTH_LONG).show();
+
+				} else {
+					Intent e = new Intent(
+							"com.edu.udea.drai.bitacoras.Lista_tareas");
+					e.putExtra("tareas", new DataPass(tareasObj));
+					startActivity(e);
+				}
+
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+	}
+
+	protected Boolean estaConectado() {
+		if (conectadoWifi()) {
+			return true;
+		} else {
+			if (conectadoRedMovil()) {
+				return true;
+			} else {
+				showAlertDialog(MainActivity.this, "Conexion a Internet",
+						"Tu Dispositivo no tiene Conexion a Internet.");
+
+
+					
+				/*Toast.makeText(
+						getApplicationContext(),
+						"No existe conexion a Internet.  \n"
+								+ "Por favor verifique la conexion y pruebe ingresar nuevamente",
+						Toast.LENGTH_LONG).show();*/
+				return false;
+			}
+		}
+	}
+
+	protected Boolean conectadoWifi() {
+		ConnectivityManager connectivity = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+		if (connectivity != null) {
+			NetworkInfo info = connectivity
+					.getNetworkInfo(ConnectivityManager.TYPE_WIFI);
+			if (info != null) {
+				if (info.isConnected()) {
+					return true;
+				}
+			}
+		}
+		return false;
+	}
+
+	protected Boolean conectadoRedMovil() {
+		ConnectivityManager connectivity = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+		if (connectivity != null) {
+			NetworkInfo info = connectivity
+					.getNetworkInfo(ConnectivityManager.TYPE_MOBILE);
+			if (info != null) {
+				if (info.isConnected()) {
+					return true;
+				}
+			}
+		}
+		return false;
 	}
 	
-	
+	public void showAlertDialog(Context context, String title, String message) {
+		AlertDialog alertDialog = new AlertDialog.Builder(context).create();
+		 
+		alertDialog.setTitle(title);
+		 
+		alertDialog.setMessage(message);
+		alertDialog.setButton("OK", new DialogInterface.OnClickListener() {
+			public void onClick(DialogInterface dialog, int which) {
+			}
+			});
+
+		alertDialog.show();
+		}
+
+
+		
 
 }
